f1b23eb7a2b5a10349726cd224abe50024d99e91
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2018-05-25 07:38:04 +0200

    selekcja komponentów Semantic UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,19 @@ import VueCookie from "vue-cookie"
 import VueAnalytics from "vue-analytics"
 import axios from "axios"
 
-require("semantic-ui-css/semantic.css")
+require("semantic-ui-css/components/button.min.css")
+require("semantic-ui-css/components/container.min.css")
+require("semantic-ui-css/components/dimmer.min.css")
+require("semantic-ui-css/components/divider.min.css")
+require("semantic-ui-css/components/grid.min.css")
+require("semantic-ui-css/components/header.min.css")
+require("semantic-ui-css/components/icon.min.css")
+require("semantic-ui-css/components/list.min.css")
+require("semantic-ui-css/components/loader.min.css")
+require("semantic-ui-css/components/menu.min.css")
+require("semantic-ui-css/components/reset.min.css")
+require("semantic-ui-css/components/segment.min.css")
+require("semantic-ui-css/components/site.min.css")
 
 import App from "./components/App"
 import Global from "./mixins/Global"
